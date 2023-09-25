4c44ae68672e7babe027445d58b7cb4680d42eb7
frustrated018 <niloyd018@gmail.com>
2023-09-26 00:11:31 +0600

    Added the show all show less button in the donation page

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import DonationCard from "./DonationCard";
 
 const Donation = () => {
+  const [showAll, setShowALl] = useState(false);
   const [donations, setDonations] = useState([]);
-
   const [noDono, setNoDono] = useState(false);
 
+  console.log(showAll);
   useEffect(() => {
     const donatedItem = JSON.parse(localStorage.getItem("donated"));
 
@@ -18,13 +19,34 @@ const Donation = () => {
 
   return (
     <div>
-      {noDono ?
-       (<h2 className=" h-[80vh] flex justify-center items-center text-3xl text-red-600">{noDono}</h2>) 
-       : 
-       (<div className="grid grid-cols-2 gap-5">
-        { donations.map((dono, idx) => <DonationCard key={idx} dono={dono}></DonationCard>)}
-       </div>)
-      }
+      {noDono ? (
+        <h2 className=" h-[80vh] flex justify-center items-center text-3xl text-red-600">
+          {noDono}
+        </h2>
+      ) : (
+        <div className="grid grid-cols-2 gap-5">
+          {showAll
+            ? donations.map((dono, idx) => (
+                <DonationCard key={idx} dono={dono}></DonationCard>
+              ))
+            : donations
+                .slice(0, 4)
+                .map((dono, idx) => (
+                  <DonationCard key={idx} dono={dono}></DonationCard>
+                ))}
+        </div>
+      )}
+
+      {donations.length > 4 && (
+        <div className="flex justify-center items-center">
+          <button
+            onClick={() => setShowALl(!showAll)}
+            className="w-[110px] h-12 bg-green-600 rounded-lg my-10 text-white text-base font-semibold"
+          >
+            {showAll ? "Show less" : "Show All"}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
