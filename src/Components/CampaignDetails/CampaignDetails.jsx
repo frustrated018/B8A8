ff9f2b4f8f2a41ff4d00dc974797569e806c7994
frustrated018 <niloyd018@gmail.com>
2023-09-25 15:15:25 +0600

    Added the black overlay

--- src/Components/CampaignDetails/CampaignDetails.jsx
+++ src/Components/CampaignDetails/CampaignDetails.jsx
@@ -15,19 +15,26 @@ const CampaignDetails = () => {
     setCampaign(clickedCampaign);
   }, [id, campaigns]);
 
-  const {
-    text_color,
-    campaign_description,
-    campaign_name,
-    img,
-  } = campaign || {};
+  const { text_color, campaign_description, campaign_name, img } =
+    campaign || {};
 
   return (
     <div>
-        <div>
-            <img src={img} className="w-full" />
-            <button style={{backgroundColor: text_color}} className="">Donate</button>
+      <div style={{ position: "relative" }}>
+        <img src={img} className="w-full static" />
+        <div
+          style={{ backgroundColor: "rgba(11, 11, 11, 0.50)" }}
+          className="h-[130px] rounded-bl-lg rounded-br-lg absolute bottom-0 left-0 w-full"
+        >
+          <button
+            style={{ backgroundColor: text_color }}
+            className="mt-4 px-6 py-2 rounded"
+          >
+            Donate
+          </button>
         </div>
+      </div>
+
       <h2 className="text-neutral-950 text-[40px] font-bold">
         {campaign_name}
       </h2>
