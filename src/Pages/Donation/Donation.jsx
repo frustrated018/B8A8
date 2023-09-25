40a5d2a2fc5ca290f482823bfe1cec88d3d7ce70
frustrated018 <niloyd018@gmail.com>
2023-09-25 21:13:25 +0600

    Made cards for donation page

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
+import DonationCard from "./DonationCard";
 
 const Donation = () => {
-    return (
-        <div>
-            <h2 className="text-center text-3xl text-green-600 my-20">Donation page</h2>
-        </div>
-    );
+  const [donations, setDonations] = useState([]);
+
+  const [noDono, setNoDono] = useState(false);
+
+  useEffect(() => {
+    const donatedItem = JSON.parse(localStorage.getItem("donated"));
+
+    if (donatedItem) {
+      setDonations(donatedItem);
+    } else {
+      setNoDono("NO DONATIONS YET");
+    }
+  }, []);
+
+  return (
+    <div>
+      {noDono ?
+       (<h2 className=" h-[80vh] flex justify-center items-center text-3xl text-red-600">{noDono}</h2>) 
+       : 
+       (<div className="grid grid-cols-2 gap-5">
+        { donations.map((dono, idx) => <DonationCard key={idx} dono={dono}></DonationCard>)}
+       </div>)
+      }
+    </div>
+  );
 };
 
 export default Donation;
---
+++ src/Pages/Donation/DonationCard.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+const DonationCard = ({ dono }) => {
+  const { background_color, badge_bg_color, category, text_color, campaign_name, img } =
+    dono || {};
+  return (
+    <div>
+      <div style={{ backgroundColor: background_color }} className="relative flex w-full max-w-[48rem] flex-row rounded-xl  bg-clip-bordershadow-md">
+        <div className="relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-clip-border">
+          <img src={img} alt="image" className="h-full w-full object-cover" />
+        </div>
+        <div className="p-6">
+          <div style={{ color: text_color }} className="text-sm font-medium">
+            <span
+              style={{ backgroundColor: badge_bg_color }}
+              className=" border px-2.5 py-1 rounded"
+            >
+              {category}
+            </span>
+          </div>
+          <h6 className=" block text-2xl font-semibold  leading-relaxed tracking-normal text-neutral-900 antialiased mt-1">
+            {campaign_name}
+          </h6>
+          <button
+            style={{ backgroundColor: text_color }}
+            className=" h-10 w-[141px] px-4 py-[9px] rounded text-white  font-semibold "
+          >
+            View details
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+DonationCard.propTypes = {
+  dono: PropTypes.object,
+};
+export default DonationCard;
