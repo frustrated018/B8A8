f3700433ee9100cd5b7cd8e05bf2801a0fff7868
frustrated018 <niloyd018@gmail.com>
2023-09-27 18:55:07 +0600

    Updated the logo img src from ../to a direct link using imagebb

--- src/Components/Header/Navbar/Logo.jsx
+++ src/Components/Header/Navbar/Logo.jsx
@@ -1,10 +1,13 @@
-
 const Logo = () => {
-    return (
-        <div>
-            <img className="w-[140px] h-[38.70px]" src="../../../src/assets/Resources/Logo.png" alt="Logo" />
-        </div>
-    );
+  return (
+    <div>
+      <img
+        className="w-[140px] h-[38.70px]"
+        src="https://i.ibb.co/0JMZ9tF/Logo.png"
+        alt="Logo"
+      />
+    </div>
+  );
 };
 
 export default Logo;
